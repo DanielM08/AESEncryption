421169e8d06373e0384fe661ee0d632b231a2820
daniel.carvalho <daniel.carvalho@maxmilhas.com.br>
2023-10-01 13:01:45 -0300

    refactor: use new EncryptionManager class

--- pycryptosimpleaes.py
+++ pycryptosimpleaes.py
@@ -1,128 +1,125 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
-from simple_aes_cipher import AESCipher, generate_secret_key
+from encryptionManager import EncryptionManager
 
 root = tk.Tk()
 root.title("Cryptography")
 
-
 class Win1:
+  def __init__(self, master):
+    self.encryptionManager = EncryptionManager()
 
-    def __init__(self, master):
-        self.master = master
-        self.master.geometry("580x640+10+10")
-        self.img = ImageTk.PhotoImage(Image.open("crypt.jpg").resize((580, 260)))
-        self.l0 = tk.Label(self.master, text='Encryption using simple AES cipher', font=("times", 24, "bold"), bg='blue',
-                      fg='white')
-        self.l0.pack(side="top", fill="both", expand="yes")
-        self.panel = tk.Label(self.master, image=self.img)
-        self.panel.pack(side="top", fill="both", expand="yes")
-        self.frame = tk.Frame(self.master)
-        self.l1 = tk.Label(self.master,text='Protect your valuable data from hackers by encryption',font=("times",16,"bold"),fg='blue')
-        self.l1.pack()
-        self.butnew("Click to Encrypt",  Win2)
-        self.butnew("Click to Decrypt",  Win3)
-        self.frame.pack(expand="true")
+    self.master = master
+    self.master.geometry("580x640+10+10")
+    self.img = ImageTk.PhotoImage(Image.open("crypt.jpg").resize((580, 260)))
+    self.l0 = tk.Label(self.master, text='Encryption using simple AES cipher', font=("times", 24, "bold"), bg='blue',
+                  fg='white')
+    self.l0.pack(side="top", fill="both", expand="yes")
+    self.panel = tk.Label(self.master, image=self.img)
+    self.panel.pack(side="top", fill="both", expand="yes")
+    self.frame = tk.Frame(self.master)
+    self.l1 = tk.Label(self.master,text='Protect your valuable data from hackers by encryption',font=("times",16,"bold"),fg='blue')
+    self.l1.pack()
+    self.butnew("Click to Encrypt",  Win2)
+    self.butnew("Click to Decrypt",  Win3)
+    self.frame.pack(expand="true")
 
+  def butnew(self, text, _class):
+    tk.Button(self.frame, text=text,command=lambda: self.new_window( _class),width=15,height=3,font=("times",14,"bold"), bg="purple",fg="yellow").pack(side="left",padx=15)
 
-    def butnew(self, text, _class):
-        tk.Button(self.frame, text=text,command=lambda: self.new_window( _class),width=15,height=3,font=("times",14,"bold"), bg="purple",fg="yellow").pack(side="left",padx=15)
-
-    def new_window(self,  _class):
-        self.new = tk.Toplevel(self.master)
-        _class(self.new)
+  def new_window(self,  _class):
+    self.new = tk.Toplevel(self.master)
+    _class(self.new, self.encryptionManager)
 
 
 class Win2:
-    def encrypts(self):
-        message = self.t1.get("1.0", tk.END)
-        pass_phrase= self.tkey.get()
-        secret_key=generate_secret_key(pass_phrase)
-        print(secret_key)
-        cipher = AESCipher(secret_key)
-        ciphertext = cipher.encrypt(message)
+  def encrypts(self):
+    message = self.t1.get("1.0", tk.END) # Mensagem a ser criptografada
+    secret_key= self.tkey.get() # Chave secreta de 16 bytes
 
-        self.t2.delete('1.0', tk.END)
-        self.t2.insert("1.0", ciphertext)
+    ciphertext = self.encryptionManager.encrypts(message, secret_key)
 
-    def __init__(self, master):
-        self.master = master
-        self.master.geometry("550x500+470+150")
-        self.master.title("Encrypt your data")
-        self.master.configure(background="palegreen")
-        self.frame = tk.Frame(self.master,borderwidth=2)
-        self.l1 = tk.Label(self.master, text='Plain Text (Text to Encrypt)',font=("times", 16, "bold"),bg="palegreen", fg='red2')
-        self.l1.pack(expand="yes")
-        self.t1 = tk.Text(self.master, height=5, width=40, borderwidth=1,relief="solid")
-        self.t1.pack(expand="yes")
+    self.t2.delete('1.0', tk.END)
+    self.t2.insert("1.0", ciphertext)
 
-        self.lkey = tk.Label(self.master, text='Secret key', font=("times", 16, "bold"), bg="palegreen", fg='red2')
-        self.lkey.pack(expand="yes")
-        self.tkey = tk.Entry(self.master, width=55,show="*", borderwidth=1, relief="solid")
-        self.tkey.pack(expand="yes")
+  def __init__(self, master, encryptionManager):
+    self.encryptionManager = encryptionManager
 
-        self.b1 = tk.Button(self.master,text="Encrypt",command=self.encrypts,font=("times", 16, "bold"),bg="maroon",fg="white",width=10)
-        self.b1.pack(expand="yes")
-        self.l2 = tk.Label(self.master, text='Cipher Text (Encrypted Text)', font=("times", 16, "bold"),bg="palegreen", fg='red2')
-        self.l2.pack(expand="yes")
-        self.t2 = tk.Text(self.master, height=8, width=40, borderwidth=1, relief="solid",state="normal")
-        self.t2.pack(expand="yes")
-        self.quit = tk.Button(self.frame, text="Close", command=self.close_window,font=("times", 16, "bold"),bg="maroon",fg="white",width=10)
-        self.quit.pack()
-        self.frame.pack(expand="true")
+    self.master = master
+    self.master.geometry("550x500+470+150")
+    self.master.title("Encrypt your data")
+    self.master.configure(background="palegreen")
+    self.frame = tk.Frame(self.master,borderwidth=2)
+    self.l1 = tk.Label(self.master, text='Plain Text (Text to Encrypt)',font=("times", 16, "bold"),bg="palegreen", fg='red2')
+    self.l1.pack(expand="yes")
+    self.t1 = tk.Text(self.master, height=5, width=40, borderwidth=1,relief="solid")
+    self.t1.pack(expand="yes")
 
-    def close_window(self):
-        self.master.destroy()
+    self.lkey = tk.Label(self.master, text='Secret key - 16 bytes', font=("times", 16, "bold"), bg="palegreen", fg='red2')
+    self.lkey.pack(expand="yes")
+    self.tkey = tk.Entry(self.master, width=16, borderwidth=1, relief="solid")
+    self.tkey.pack(expand="yes")
+
+    self.b1 = tk.Button(self.master,text="Encrypt",command=self.encrypts,font=("times", 16, "bold"),bg="maroon",fg="white",width=10)
+    self.b1.pack(expand="yes")
+    self.l2 = tk.Label(self.master, text='Cipher Text (Encrypted Text)', font=("times", 16, "bold"),bg="palegreen", fg='red2')
+    self.l2.pack(expand="yes")
+    self.t2 = tk.Text(self.master, height=8, width=40, borderwidth=1, relief="solid",state="normal")
+    self.t2.pack(expand="yes")
+    self.quit = tk.Button(self.frame, text="Close", command=self.close_window,font=("times", 16, "bold"),bg="maroon",fg="white",width=10)
+    self.quit.pack()
+    self.frame.pack(expand="true")
+
+  def close_window(self):
+    self.master.destroy()
 
 
 class Win3:
 
-    def decrypts(self):
+  def decrypts(self):
+    cipherText = self.t1.get("1.0", tk.END)
+    cipherText = cipherText.strip()
+    secret_key = self.tkey.get()
 
-        ciphertext = self.t1.get("1.0", tk.END)
-        ciphertext = ciphertext.strip()
-        pass_phrase = self.tkey.get()
-        secret_key = generate_secret_key(pass_phrase)
-        print(secret_key)
-        cipher = AESCipher(secret_key)
-        message = cipher.decrypt(ciphertext)
+    originalMessage = self.encryptionManager.decrypts(cipherText, secret_key)
 
-        self.t2.delete('1.0', tk.END)
-        self.t2.insert("1.0", message)
+    self.t2.delete('1.0', tk.END)
+    self.t2.insert("1.0", originalMessage)
 
-    def __init__(self, master):
-        self.master = master
-        self.master.geometry("550x500+900+150")
-        self.master.title("Decrypt your data")
-        self.master.configure(background="pink")
-        self.frame = tk.Frame(self.master, borderwidth=2)
-        self.l1 = tk.Label(self.master, text='Cipher Text (Encrypted data)', font=("times", 16, "bold"), bg="pink",fg='blue')
-        self.l1.pack(expand="yes")
-        self.t1 = tk.Text(self.master, height=8, width=40, borderwidth=1, relief="solid")
-        self.t1.pack(expand="yes")
+  def __init__(self, master, encryptionManager):
+    self.encryptionManager = encryptionManager
 
-        self.lkey = tk.Label(self.master, text='Secret key', font=("times", 16, "bold"), bg="pink", fg='blue')
-        self.lkey.pack(expand="yes")
-        self.tkey = tk.Entry(self.master, width=55, show="*", borderwidth=1, relief="solid")
-        self.tkey.pack(expand="yes")
+    self.master = master
+    self.master.geometry("550x500+900+150")
+    self.master.title("Decrypt your data")
+    self.master.configure(background="pink")
+    self.frame = tk.Frame(self.master, borderwidth=2)
+    self.l1 = tk.Label(self.master, text='Cipher Text (Encrypted data)', font=("times", 16, "bold"), bg="pink",fg='blue')
+    self.l1.pack(expand="yes")
+    self.t1 = tk.Text(self.master, height=8, width=40, borderwidth=1, relief="solid")
+    self.t1.pack(expand="yes")
 
-        self.b1 = tk.Button(self.master, text="Decrypt", command=self.decrypts, font=("times", 16, "bold"), bg="blue",
-                            fg="white", width=10)
-        self.b1.pack(expand="yes")
-        self.l2 = tk.Label(self.master, text='Plain Text(Original message)', font=("times", 16, "bold"), bg="pink",
-                           fg='blue')
-        self.l2.pack(expand="yes")
-        self.t2 = tk.Text(self.master, height=5, width=40, borderwidth=1, relief="solid", state="normal")
-        self.t2.pack(expand="yes")
-        self.quit = tk.Button(self.frame, text="Close", command=self.close_window, font=("times", 16, "bold"),
-                              bg="blue", fg="white", width=10)
-        self.quit.pack()
-        self.frame.pack(expand="true")
+    self.lkey = tk.Label(self.master, text='Secret key - 16 bytes', font=("times", 16, "bold"), bg="pink", fg='blue')
+    self.lkey.pack(expand="yes")
+    self.tkey = tk.Entry(self.master, width=16, borderwidth=1, relief="solid")
+    self.tkey.pack(expand="yes")
 
-    def close_window(self):
-        self.master.destroy()
+    self.b1 = tk.Button(self.master, text="Decrypt", command=self.decrypts, font=("times", 16, "bold"), bg="blue",
+                        fg="white", width=10)
+    self.b1.pack(expand="yes")
+    self.l2 = tk.Label(self.master, text='Plain Text(Original message)', font=("times", 16, "bold"), bg="pink",
+                        fg='blue')
+    self.l2.pack(expand="yes")
+    self.t2 = tk.Text(self.master, height=5, width=40, borderwidth=1, relief="solid", state="normal")
+    self.t2.pack(expand="yes")
+    self.quit = tk.Button(self.frame, text="Close", command=self.close_window, font=("times", 16, "bold"),
+                          bg="blue", fg="white", width=10)
+    self.quit.pack()
+    self.frame.pack(expand="true")
 
+  def close_window(self):
+    self.master.destroy()
 
 app = Win1(root)
 root.mainloop()
